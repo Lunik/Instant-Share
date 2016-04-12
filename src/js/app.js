d8aacc14b04edbbce17d2581664e54a7749f84f8
Lunik <guillaume.lunik@gmail.com>
2016-04-12 15:56:20 +0200

    switch from JS to JQuery

--- src/js/app.js
+++ src/js/app.js
@@ -20,39 +20,50 @@ function getHash(){
 
 // Initialise seed torrent
 function initHolder(){
-  var holder = document.getElementsByClassName('holder')[0];
-  var upload = document.getElementsByClassName('upload')[0];
-  var file_name = document.getElementsByClassName('filename')[0]
-  var upload_but = document.getElementsByClassName('upload-but')[0];
-  var state = document.getElementsByClassName('status')[0];
+  var holder = $('.holder');
+  var upload = $('.holder .upload .button');
+  var file_name = $('.holder .filename');
+  var upload_but = $('.holder .upload-but');
+  var state = $('.status');
 
   if (typeof window.FileReader === 'undefined') {
-    state.id = 'fail';
-    state.innerHTML = 'Instant Share indisponible';
+    state.attr('id','fail');
+    state.text('Instant Share indisponible');
   } else {
-    state.id = 'success';
-    state.innerHTML = 'Instant Share disponible';
+    state.attr('id','success');
+    state.text('Instant Share disponible');
   }
 
-  holder.ondragover = function () { this.id = 'hover'; return false; };
-  holder.ondragend = function () { this.id = ''; return false; };
-  holder.ondrop = function (e) {
-    this.id = '';
-    e.preventDefault();
+  holder.on('dragover',function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.id = 'hover'; return false;
+  });
 
-    var file = e.dataTransfer.files[0]
+  holder.on('dragleave',function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.id = ''; return false;
+  });
+
+  holder.on('drop', function (event) {
+    this.id = '';
+    event.preventDefault();
+    event.stopPropagation();
+
+    var file = event.originalEvent.dataTransfer.files[0]
 
     seed(file)
-  };
+  });
 
-  upload.onclick = function(){
-    upload_but.click()
-  }
+  upload.click(function(){
+    upload_but.trigger('click')
+  })
 
-  upload_but.onchange = function(e){
-    file_name.innerHTML = this.files[0].name
-    seed(this.files[0]);
-  }
+  upload_but.on('change', function(){
+    file_name.text(this.files[0].name)
+    //seed(this.files[0]);
+  })
 }
 
 
@@ -64,10 +75,10 @@ function seed(file){
 
 // Initialise event on torrent
 function initTorrent(torrent){
-  var holder = document.getElementsByClassName('holder')[0];
+  var holder = $('.holder');
   torrent.on('done', function(){
     console.log('torrent finished downloading');
-    holder.style.background = ""
+    holder.css('background','');
   })
 
   torrent.on('wire', function (wire) {
@@ -91,8 +102,9 @@ function download(hash){
 
 // Callback on torrent finish
 function onTorrentDownload(torrent){
-  var holder = document.getElementsByClassName('holder')[0];
-  holder.style.background =  'no-repeat center url("src/css/dashinfinity.gif")';
+  var holder = $('.holder');
+  holder.css('background','no-repeat center url("src/css/dashinfinity.gif")');
+
   console.log("Downloadind "+torrent.name)
 
   initTorrent(torrent)
@@ -107,7 +119,6 @@ function cleanBody(){
 
 // Callback function when torrent is seeding
 function onTorrentSeed(torrent){
-  var holder = document.getElementsByClassName('holder')[0];
   console.log("Seeding "+torrent.name)
   console.log("Hash: "+torrent.infoHash)
 
@@ -120,34 +131,34 @@ function onTorrentSeed(torrent){
 
 // Show the input with the current url
 function showInputUrl(url){
-  document.getElementsByClassName('url-input')[0].value = url;
-  document.getElementsByClassName('url-input')[0].hidden = false
+  $('.url-input').val(url);
+  $('.url-input').show()
 }
 
 // Show the download button for downloading the file
 function showDownloadButton(fileName, url){
-  var but = document.getElementsByClassName('download-url')[0]
-  but.innerHTML = "Télécharger "+fileName
-  but.href = url
-  but.download = fileName
+  var but = $('.download-url');
+  but.text("Télécharger "+fileName);
+  but.attr('href',url)
+  but.attr('download',fileName)
 }
 
 // append a torrent to the holder
 function appendHolder(torrent){
-  var holder = document.getElementsByClassName('holder')[0];
-  holder.innerHTML = ""
+  var holder = $('.holder');
+  holder.text('')
   torrent.files.forEach(function(file){
     file.getBlobURL(function (err, url) {
-      holder.onclick = function(){ window.open(url,'_blank')}
+      holder.click(function(){ window.open(url,'_blank')})
       showDownloadButton(file.name, url)
       showInputUrl(document.location.hostname+"/#"+torrent.infoHash)
-      file.appendTo(holder)
+      file.appendTo('.holder')
     })
   })
 }
 
 // Update value of peer
 function updatePeer(peerNum){
-  var peer = document.getElementsByClassName('peer')[0]
-  peer.innerHTML = "Peers: "+peerNum;
+  var peer = $('.peer')
+  peer.text("Peers: "+peerNum);
 }
