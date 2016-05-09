02bf6101583dc7b392f636dff0b7952da67e60aa
steefmin <steefmin@gmail.com>
2016-05-09 16:21:35 +0200

    added semicolons

--- src/js/app.js
+++ src/js/app.js
@@ -1,212 +1,202 @@
 // Get Hash on hashchange
 $(window).bind('hashchange', function () {
-  getHash()
+  getHash();
 })
 
 // Get Hash on loading page
 $(document).ready(function () {
-  initHolder()
-  getHash()
-})
+  initHolder();
+  getHash();
+});
 
 // Get the hash and start torrent if there is an hash
 function getHash () {
-  var hash = window.location.hash.substring(1)
-  hash = cleanHash(hash)
+  var hash = window.location.hash.substring(1);
+  hash = cleanHash(hash);
   if (hash.length > 0) {
-    console.log('New Hash: ' + hash)
-    var $holder = $('.holder')
-    $holder.css('background', 'no-repeat center url("src/css/dashinfinity.gif")')
-    download(hash)
+    console.log('New Hash: ' + hash);
+    var $holder = $('.holder');
+    $holder.css('background', 'no-repeat center url("src/css/dashinfinity.gif")');
+    download(hash);
   }
 }
 
 // turn magnet into hash
 function cleanHash (hash) {
-  var r = new RegExp('.*:')
-  var r2 = new RegExp('&.*')
-
-  return hash.replace(r, '').replace(r2, '')
+  var r = new RegExp('.*:');
+  var r2 = new RegExp('&.*');
+  return hash.replace(r, '').replace(r2, '');
 }
 
 // Initialise seed torrent
 function initHolder () {
-  var $holder = $('.holder')
-  var $upload = $('.holder .upload .button')
-  var $fileName = $('.holder .filename')
-  var $uploadBut = $('.holder .upload-but')
-  var $state = $('.status')
+  var $holder = $('.holder');
+  var $upload = $('.holder .upload .button');
+  var $fileName = $('.holder .filename');
+  var $uploadBut = $('.holder .upload-but');
+  var $state = $('.status');
 
   if (typeof window.FileReader === 'undefined') {
-    $state.attr('id', 'fail')
-    $state.text('Instant Share Off-line')
+    $state.attr('id', 'fail');
+    $state.text('Instant Share Off-line');
   } else {
-    $state.attr('id', 'success')
-    $state.text('Instant Share On-line')
+    $state.attr('id', 'success');
+    $state.text('Instant Share On-line');
   }
 
   $holder.on('dragover', function (event) {
-    event.preventDefault()
-    event.stopPropagation()
-    this.id = 'hover'; return false
-  })
+    event.preventDefault();
+    event.stopPropagation();
+    this.id = 'hover'; return false;
+  });
 
   $holder.on('dragleave', function (event) {
-    event.preventDefault()
-    event.stopPropagation()
-    this.id = ''; return false
-  })
+    event.preventDefault();
+    event.stopPropagation();
+    this.id = ''; return false;
+  });
 
   $holder.on('drop', function (event) {
-    this.id = ''
-    event.preventDefault()
-    event.stopPropagation()
-
-    var file = event.originalEvent.dataTransfer.files[0]
-
-    seed(file)
-  })
+    this.id = '';
+    event.preventDefault();
+    event.stopPropagation();
+    var file = event.originalEvent.dataTransfer.files[0];
+    seed(file);
+  });
 
   $upload.click(function () {
-    $uploadBut.trigger('click')
-  })
+    $uploadBut.trigger('click');
+  });
 
   $uploadBut.on('change', function () {
-    $fileName.text(this.files[0].name)
-    seed(this.files[0])
-  })
+    $fileName.text(this.files[0].name);
+    seed(this.files[0]);
+  });
 }
 
 function seed (file) {
-  console.log(file)
-  var client = new WebTorrent()
-  client.seed(file, {announce:['ws://h.steefmin.xyz:8000']}, onTorrentSeed)
+  console.log(file);
+  var client = new WebTorrent();
+  client.seed(file, {announce:['ws://h.steefmin.xyz:8000']}, onTorrentSeed);
 }
 
 // Initialise event on torrent
 function initTorrent (torrent) {
-  var $holder = $('.holder')
+  var $holder = $('.holder');
   torrent.on('done', function () {
-    console.log('torrent finished downloading')
-	updatePeer(torrent.numPeers)
-    updateData(torrent.uploaded, torrent.downloaded, torrent.uploadSpeed, torrent.downloadSpeed)
-    $holder.css('background', '')
-  })
+    console.log('torrent finished downloading');
+	updatePeer(torrent.numPeers);
+    updateData(torrent.uploaded, torrent.downloaded, torrent.uploadSpeed, torrent.downloadSpeed);
+    $holder.css('background', '');
+  });
 
   torrent.on('wire', function (wire) {
-    console.log('new peer')
-    updatePeer(torrent.numPeers)
-  })
+    console.log('new peer');
+    updatePeer(torrent.numPeers);
+  });
 
   torrent.on('download', function (chunkSize) {
-    updateData(torrent.uploaded, torrent.downloaded, torrent.uploadSpeed, torrent.downloadSpeed)
-  })
+    updateData(torrent.uploaded, torrent.downloaded, torrent.uploadSpeed, torrent.downloadSpeed);
+  });
 
   torrent.on('upload', function (data) {
-    updateData(torrent.uploaded, torrent.downloaded, torrent.uploadSpeed, torrent.downloadSpeed)
-  })
+    updateData(torrent.uploaded, torrent.downloaded, torrent.uploadSpeed, torrent.downloadSpeed);
+  });
 }
 
 // Download a torrent
 function download (hash) {
-  cleanBody()
-  var client = new WebTorrent()
+  cleanBody();
+  var client = new WebTorrent();
   client.add({
     infoHash: hash,
     announce: ['ws://h.steefmin.xyz:8000']
-  }, onTorrentDownload)
+  }, onTorrentDownload);
 }
 
 // Callback on torrent finish
 function onTorrentDownload (torrent) {
-  console.log('Downloading ' + torrent.name)
-
-  initTorrent(torrent)
-  
-  updatePeer(torrent.numPeers)
-
-  appendHolder(torrent)
+  console.log('Downloading ' + torrent.name);
+  initTorrent(torrent);
+  updatePeer(torrent.numPeers);
+  appendHolder(torrent);
 }
 
 // Clean holder body
 function cleanBody () {
-  $('.holder').html('')
+  $('.holder').html('');
 }
 
 // Callback function when torrent is seeding
 function onTorrentSeed (torrent) {
-  console.log('Seeding ' + torrent.name)
-  console.log('Hash: ' + torrent.infoHash)
-
-  initTorrent(torrent)
-  
-  updatePeer(torrent.numPeers)
-
-  appendHolder(torrent)
-
-  prompt('Share this link:', document.location.hostname + '/#' + torrent.infoHash)
+  console.log('Seeding ' + torrent.name);
+  console.log('Hash: ' + torrent.infoHash);
+  initTorrent(torrent);
+  updatePeer(torrent.numPeers);
+  appendHolder(torrent);
+  prompt('Share this link:', document.location.hostname + '/#' + torrent.infoHash);
   $(window).bind('beforeunload', function () {
-    return "You are still sharing a file. Are you realy sure you want to quit. You won't be this file anymore."
-  })
+    return "You are still sharing a file. Are you realy sure you want to quit. You won't be this file anymore.";
+  });
 }
 
 // Show the input with the current url
 function showInputUrl (url) {
-  $('.url-input').val(url)
-  $('.share-link').show()
+  $('.url-input').val(url);
+  $('.share-link').show();
 }
 
 // Show the download button for downloading the file
 function showDownloadButton (fileName, url) {
-  var $but = $('.download-url')
-  $but.text('Download ' + fileName)
-  $but.attr('href', url)
-  $but.attr('download', fileName)
+  var $but = $('.download-url');
+  $but.text('Download ' + fileName);
+  $but.attr('href', url);
+  $but.attr('download', fileName);
 }
 
 // append a torrent to the holder
 function appendHolder (torrent) {
-  var $holder = $('.holder')
-  $holder.text('')
+  var $holder = $('.holder');
+  $holder.text('');
   torrent.files.forEach(function (file) {
-    file.appendTo('.holder')
+    file.appendTo('.holder');
     file.getBlobURL(function (err, url) {
       if (err) {
-        console.log(err)
+        console.log(err);
       }
-      showDownloadButton(file.name, url)
-      showInputUrl(document.location.hostname + '/#' + torrent.infoHash)
-    })
-  })
+      showDownloadButton(file.name, url);
+      showInputUrl(document.location.hostname + '/#' + torrent.infoHash);
+    });
+  });
 }
 
 // bytes to formated data
 function formatData (bytes) {
-  var sizes = ['B', 'kB', 'MB', 'GB', 'TB']
-  if (bytes === 0) return '0 B'
-  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
-  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
+  var sizes = ['B', 'kB', 'MB', 'GB', 'TB'];
+  if (bytes === 0) return '0 B';
+  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }
 
 // bytes to formated data
 function formatSpeed (bits) {
-  var sizes = ['b/s', 'kb/s', 'Mb/s', 'Gb/s', 'Tb/s']
-  if (bits === 0) return '0 b/s'
-  var i = parseInt(Math.floor(Math.log(bits) / Math.log(1024)), 10)
-  return Math.round(bits / Math.pow(1024, i), 2) + ' ' + sizes[i]
+  var sizes = ['b/s', 'kb/s', 'Mb/s', 'Gb/s', 'Tb/s'];
+  if (bits === 0) return '0 b/s';
+  var i = parseInt(Math.floor(Math.log(bits) / Math.log(1024)), 10);
+  return Math.round(bits / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }
 
 
 // Update value of peer
 function updatePeer (peerNum) {
-  var $peer = $('.torrent-infos .peer p')
-  $peer.text(peerNum)
+  var $peer = $('.torrent-infos .peer p');
+  $peer.text(peerNum);
 }
 
 // update the value of downloaded bytes
 function updateData (upBytes, downBytes, upSpeed, downSpeed) {
-  var $upData = $('.torrent-infos .uploaded-data p')
-  $upData.text(formatData(upBytes)+" @"+formatSpeed(upSpeed))
-  var $downData = $('.torrent-infos .downloaded-data p')
-  $downData.text(formatData(downBytes)+" @"+formatSpeed(downSpeed))
+  var $upData = $('.torrent-infos .uploaded-data p');
+  $upData.text(formatData(upBytes)+" @"+formatSpeed(upSpeed));
+  var $downData = $('.torrent-infos .downloaded-data p');
+  $downData.text(formatData(downBytes)+" @"+formatSpeed(downSpeed));
 }
